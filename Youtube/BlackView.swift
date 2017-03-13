9f592924752ad4d3bfd3d1f207f0228534589df8
Sai Munesh <sai.munesh@nbcuni.com>
2017-03-13 16:45:42 -0400

    In progress: Collection view

--- Youtube/BlackView.swift
+++ Youtube/BlackView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BlackView: UIView {
+class BlackView: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     private var height: CGFloat!
     private var width: CGFloat!
@@ -21,10 +21,12 @@ class BlackView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    let collectionView: UICollectionView = {
+    lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.backgroundColor = UIColor.white
+        cv.delegate = self
+        cv.dataSource = self
         return cv
     }()
     
@@ -40,26 +42,55 @@ class BlackView: UIView {
             window.addSubview(collectionView)
             height = window.frame.height/2
             width = window.frame.width
+            collectionView.register(SettingCell.self, forCellWithReuseIdentifier: "cellIDD")
             collectionView.frame = CGRect(x: 0, y: height*2, width: window.frame.width, height: height)
             
-            UIView.animate(withDuration: 0.3, animations: { [weak self] in
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: { [weak self] in
                 self?.alpha = 0.5
                 self?.collectionView.frame = CGRect(x: 0, y: (self?.height)!, width: (self?.width)!, height: (self?.height)!)
-            })
+                
+            }, completion: nil)
+            
         }
     }
     
     
     @objc private func handleDismiss() {
         
-        UIView.animate(withDuration: 0.3, animations: { [weak self] in
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: { [weak self] in
             self?.alpha = 0
             self?.collectionView.frame = CGRect(x: 0, y: (self?.height)!*2, width: (self?.width)!, height: (self?.height)!)
-
-        }) { [weak self] (_) in
-            self?.collectionView.removeFromSuperview()
             
+            }, completion: { [weak self] (_) in
+                self?.collectionView.removeFromSuperview()
+        })
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellIDD", for: indexPath) as? SettingCell {
+            cell.backgroundColor = UIColor.gray
+            return cell
         }
+        return UICollectionViewCell()
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: width, height: height/5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
 
 }
